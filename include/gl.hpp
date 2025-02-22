cb02b53a6b9cc95a30e447938cf8f507fcba137e
Yaroslav Kuznetsov <asklater@bk.ru>
2025-02-22 10:23:37 +0300

    upgraded design and fixed solid

--- include/gl.hpp
+++ include/gl.hpp
@@ -14,6 +14,7 @@
 #include <functional>
 #include <iostream>
 #include <fstream>
+#include <memory>
 
 namespace gl {
 
@@ -77,7 +78,9 @@ namespace gl {
     };
 
     class Renderer;
+    class Camera;
     class Scene;
+    class EventHandler;
 
     template <typename T>
     struct UniformSeter final {};
@@ -95,39 +98,56 @@ namespace gl {
             glfwInit();
             window_ = glfwCreateWindow(widht, height, title.data(), NULL, NULL);
             if (window_ == nullptr) {
-                throw std::runtime_error("creating of window is failed");
+                throw std::runtime_error("Failed to create a window");
             }
-            SetCallback();
 
             glfwMakeContextCurrent(window_);
+
+            if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+                throw std::runtime_error("Failed to initialize GLAD");
+            }
         }
 
         ~Window() {
+            glfwDestroyWindow(window_);
             glfwTerminate();
         }
 
-        bool IsShouldBeClosed() {
-            return glfwWindowShouldClose(window_);
-        }
+        void SetEventHandler(std::unique_ptr<EventHandler> &&handler) {
+            handler_ = std::move(handler);
+        }    
 
         GLFWwindow *Get() { return window_; }
         
-        void DrawFrames(Renderer &renderer, Scene &Scene);
+        void DrawFrames(Renderer &renderer, Scene &scene, Camera &camera);
+    
     private:
-        void SetCallback() {
-            auto callback = [] (GLFWwindow* window, int widht, int height) {
-                glViewport(0, 0, widht, height);
-            };
-
-            glfwSetFramebufferSizeCallback(window_, callback);
+        bool IsShouldBeClosed() {
+            return glfwWindowShouldClose(window_);
         }
         
         GLFWwindow* window_;
+        std::unique_ptr<EventHandler> handler_;
     }; // class Window
 
-    class Shader final {
+    class IShader {
     public:
-        Shader(int type, const std::string &file_name) {
+        virtual ~IShader() {}
+        virtual int Use() const = 0;
+        virtual void Compile(const std::string &file_name) = 0;
+    }; // class IShader
+
+    class Shader final : public IShader {
+    public:
+        Shader(int type) {
+            shader_ = glCreateShader(type);
+        }
+
+        int Use() const override {
+            return shader_;
+        }
+
+        void Compile(const std::string &file_name) override {
             std::ifstream stream(file_name);
             if (!stream.is_open()) {
                 throw std::runtime_error("shader file is not found");
@@ -136,26 +156,13 @@ namespace gl {
             std::string program_text = std::string(std::istreambuf_iterator<char>(stream), std::istreambuf_iterator<char>());
             const char *text = program_text.data();
 
-            shader_ = glCreateShader(type);
             glShaderSource(shader_, 1, &text, NULL);
             glCompileShader(shader_);
 
             int success;
             glGetShaderiv(shader_, GL_COMPILE_STATUS, &success);
             if (!success) {
-                throw std::runtime_error("shader compiling is failed");
-            }
-        }
-
-        Shader(int type, const char **program_text) {
-            shader_ = glCreateShader(type);
-            glShaderSource(shader_, 1, program_text, NULL);
-            glCompileShader(shader_);
-
-            int success;
-            glGetShaderiv(shader_, GL_COMPILE_STATUS, &success);
-            if (!success) {
-                throw std::runtime_error("shader compiling is failed");
+                throw std::runtime_error("Failed to compile shader");
             }
         }
         
@@ -163,21 +170,18 @@ namespace gl {
             glDeleteShader(shader_);
         }
 
-        int operator()() const {
-            return shader_;
-        }
     private:
         int shader_;
     }; // class Shader
 
     class Program final {
-        public:
+    public:
         Program() {
             id_ = glCreateProgram();
         }
 
-        void AttachShader(const Shader& shader_obj) {
-            glAttachShader(id_, shader_obj());
+        void AttachShader(const IShader& shader_obj) {
+            glAttachShader(id_, shader_obj.Use());
         }
 
         void Link() {
@@ -260,19 +264,27 @@ namespace gl {
 
     class Camera final {
     public:
-        Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3 target = glm::vec3(0.0f, 0.0f, 0.0f))
-            : position_(position), target_(target) {
-            Update();
+        Camera() {
+            UpdatePosition();
+        }
+
+        Camera(glm::vec3 position, glm::vec3 target, float FoV, float aspect, float near, float far)
+            : position_(position), target_(target), FoV_(FoV), aspect_(aspect), near_(near), far_(far) {
+            UpdatePosition();
         }
 
         void SetPosition(const glm::vec3 &position) {
             position_ = position;
-            Update();
+            UpdatePosition();
         }
 
         void SetTarget(const glm::vec3 &target) {
             target_ = target;
-            Update();
+            UpdatePosition();
+        }
+        
+        void SetAspect(float aspect) {
+            aspect_ = aspect;
         }
 
         glm::vec3 GetPosition() {
@@ -291,58 +303,85 @@ namespace gl {
             return right_;
         }
 
-        glm::mat4 GetView() const {
+        glm::mat4 GetViewMatrix() const {
             return glm::lookAt(position_, position_ + direction_, up_);
         }
 
+        glm::mat4 GetProjectionMatrix() const {
+            return glm::perspective(glm::radians(FoV_), aspect_, near_, far_);
+        }
+
+        void UpdateAspect() {
+            GLint viewport[4];
+            glGetIntegerv(GL_VIEWPORT, viewport);
+            aspect_ = static_cast<float>(viewport[2]) / viewport[3];
+        }
+
     private:
-        void Update() {
+        void UpdatePosition() {
             direction_ = glm::normalize(target_ - position_);
             right_ = glm::normalize(glm::cross(glm::vec3(0.0f, 1.0f, 0.0f), direction_));
             up_ = glm::cross(direction_, right_);
         }
 
-        glm::vec3 position_;
-        glm::vec3 target_;
+        float FoV_ = 45.0f;
+        float aspect_ = 800.0f / 600.0f;
+        float near_ = 0.1f;
+        float far_ = 100.0f;
+
+        glm::vec3 position_ = glm::vec3(0.0f, 0.0f, 3.0f);
+        glm::vec3 target_ = glm::vec3(0.0f, 0.0f, 0.0f);
         
         glm::vec3 direction_;
         glm::vec3 right_;
         glm::vec3 up_;
     }; // class Camera
 
-    struct EventHandler final {
-        static void UpdateEvent(Window &window, Camera &camera) {
+    class EventHandler final {
+    public:
+        EventHandler(Window &window, Camera &camera) : window_(window), camera_(camera) {
+            glfwSetWindowUserPointer(window.Get(), &camera_);
+
+            auto callback = [] (GLFWwindow* window, int widht, int height) {
+                glViewport(0, 0, widht, height);
+                Camera *camera = static_cast<Camera*>(glfwGetWindowUserPointer(window));
+                camera->SetAspect(static_cast<float>(widht) / height);
+            };
+    
+            glfwSetFramebufferSizeCallback(window_.Get(), callback);
+        }
+    
+        void UpdateEvent() {
             const float camera_speed = 0.05f;
-            if (glfwGetKey(window.Get(), GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-                glfwSetWindowShouldClose(window.Get(), true);
+            if (glfwGetKey(window_.Get(), GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+                glfwSetWindowShouldClose(window_.Get(), true);
             }
-            if (glfwGetKey(window.Get(), GLFW_KEY_W) == GLFW_PRESS || glfwGetKey(window.Get(), GLFW_KEY_UP) == GLFW_PRESS) {
-                std::cout << "UP" << std::endl;
-                camera.SetPosition(camera.GetPosition() + camera_speed * camera.GetUp());
+            if (glfwGetKey(window_.Get(), GLFW_KEY_W) == GLFW_PRESS || glfwGetKey(window_.Get(), GLFW_KEY_UP) == GLFW_PRESS) {
+                camera_.SetPosition(camera_.GetPosition() + camera_speed * camera_.GetUp());
             }    
-            if (glfwGetKey(window.Get(), GLFW_KEY_S) == GLFW_PRESS || glfwGetKey(window.Get(), GLFW_KEY_DOWN) == GLFW_PRESS) {
-                std::cout << "DOWN" << std::endl;
-                camera.SetPosition(camera.GetPosition() - camera_speed * camera.GetUp());
+            if (glfwGetKey(window_.Get(), GLFW_KEY_S) == GLFW_PRESS || glfwGetKey(window_.Get(), GLFW_KEY_DOWN) == GLFW_PRESS) {
+                camera_.SetPosition(camera_.GetPosition() - camera_speed * camera_.GetUp());
             }
-            if (glfwGetKey(window.Get(), GLFW_KEY_A) == GLFW_PRESS || glfwGetKey(window.Get(), GLFW_KEY_LEFT) == GLFW_PRESS) {
-                std::cout << "LEFT" << std::endl;
-                camera.SetPosition(camera.GetPosition() + camera_speed * camera.GetRight());
+            if (glfwGetKey(window_.Get(), GLFW_KEY_A) == GLFW_PRESS || glfwGetKey(window_.Get(), GLFW_KEY_LEFT) == GLFW_PRESS) {
+                camera_.SetPosition(camera_.GetPosition() + camera_speed * camera_.GetRight());
             }
-            if (glfwGetKey(window.Get(), GLFW_KEY_D) == GLFW_PRESS || glfwGetKey(window.Get(), GLFW_KEY_RIGHT) == GLFW_PRESS) {
-                std::cout << "RIGHT" << std::endl;
-                camera.SetPosition(camera.GetPosition() - camera_speed * camera.GetRight());
+            if (glfwGetKey(window_.Get(), GLFW_KEY_D) == GLFW_PRESS || glfwGetKey(window_.Get(), GLFW_KEY_RIGHT) == GLFW_PRESS) {
+                camera_.SetPosition(camera_.GetPosition() - camera_speed * camera_.GetRight());
             }
-            if (glfwGetKey(window.Get(), GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) {
-                if (glfwGetKey(window.Get(), GLFW_KEY_EQUAL) == GLFW_PRESS) {
-                    camera.SetPosition(camera.GetPosition() + camera_speed * camera.GetDirection());
+            if (glfwGetKey(window_.Get(), GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) {
+                if (glfwGetKey(window_.Get(), GLFW_KEY_EQUAL) == GLFW_PRESS) {
+                    camera_.SetPosition(camera_.GetPosition() + camera_speed * camera_.GetDirection());
                 }
-                else if (glfwGetKey(window.Get(), GLFW_KEY_MINUS) == GLFW_PRESS) {
-                    camera.SetPosition(camera.GetPosition() - camera_speed * camera.GetDirection());
+                else if (glfwGetKey(window_.Get(), GLFW_KEY_MINUS) == GLFW_PRESS) {
+                    camera_.SetPosition(camera_.GetPosition() - camera_speed * camera_.GetDirection());
                 }
             }
         }
-    };
 
+    private:
+        Window& window_;
+        Camera& camera_;
+    };
 
     class Scene final {
     public:
@@ -367,29 +406,30 @@ namespace gl {
     
     class Renderer final {
     public:
-        Renderer() : camera_(), program_() {
+        Renderer() : program_() {
             glEnable(GL_DEPTH_TEST);
 
-            {
-                gl::Shader vertex{GL_VERTEX_SHADER, "include/shaders/triangle.vs"};
-                gl::Shader fragment{GL_FRAGMENT_SHADER, "include/shaders/triangle.fs"};
-                program_.AttachShader(vertex);
-                program_.AttachShader(fragment);
-            }
+            gl::Shader vertex{GL_VERTEX_SHADER};
+            vertex.Compile("include/shaders/triangle.vs");            
+            gl::Shader fragment{GL_FRAGMENT_SHADER};
+            fragment.Compile("include/shaders/triangle.fs");
+
+            program_.AttachShader(vertex);
+            program_.AttachShader(fragment);
 
             program_.Link();
         }
 
         
-        void Render(Scene &scene) {
-            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        void Render(Scene &scene, const Camera &camera) {
+            glClearColor(0.5f, 0.1f, 0.5f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
             program_.Run();
     
             glm::mat4 model = glm::mat4(1.0f);
-            glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-            glm::mat4 view = camera_.GetView();
+            glm::mat4 projection = camera.GetProjectionMatrix();
+            glm::mat4 view = camera.GetViewMatrix();
             
             gl::template UniformSeter<glm::mat4>::Set(program_(), "projection", projection);
             gl::template UniformSeter<glm::mat4>::Set(program_(), "view", view);
@@ -398,27 +438,19 @@ namespace gl {
             scene.vao.Bind(scene.vao());
         }
 
-        const Camera &GetCamera() const {
-            return camera_;
-        }
-
-        Camera &GetCamera() {
-            return camera_;
-        }
-
         ~Renderer() {
             glfwTerminate();
         }
     private:
-        Camera camera_;
         Program program_;
     }; // class Renderer
 
-    void Window::DrawFrames(Renderer &renderer, Scene &scene) {
+
+    void Window::DrawFrames(Renderer &renderer, Scene &scene, Camera &camera) {
         while (!IsShouldBeClosed()) {
-            EventHandler::UpdateEvent(*this, renderer.GetCamera());
-    
-            renderer.Render(scene);
+            handler_->UpdateEvent();
+            
+            renderer.Render(scene, camera);
             glDrawArrays(GL_TRIANGLES, 0, 36);
 
             glfwSwapBuffers(window_);
