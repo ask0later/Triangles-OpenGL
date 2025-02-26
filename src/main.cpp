5e94813b04cbfbeddf19ced30088c65cde574bce
Yaroslav Kuznetsov <asklater@bk.ru>
2025-02-26 20:01:34 +0300

    refactored

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,33 @@
 constexpr int START_WIDHT = 800, START_HEIGHT = 600;
 
 int main() {
-    scene::TriangleScene tscene{};
-    tscene.Make();
-    
     gl::Window window{START_WIDHT, START_HEIGHT, "Triangle scene"};
     gl::Camera camera{};
     std::unique_ptr<gl::IEventHandler> handler = std::move(std::make_unique<gl::EventHandler>(window, camera));
     window.SetEventHandler(std::move(handler));
-
     gl::Renderer renderer{};
-    gl::Scene scene{};
+
+    size_t point_count = 0;
+    std::vector<float> data;
     
+    {
+        scene::TriangleScene tscene{};
+        tscene.Make();
+        point_count = tscene.GetPointCount();
+        auto center = tscene.GetCenter();
+        auto offset = tscene.GetRadius() / glm::tan(glm::radians(camera.GetFoV() * 0.5f));
+        camera.SetTarget(center);
+        camera.SetPosition(center + glm::vec3(0.0f, 0.0f, offset)); 
+
+        scene::GeometryData geom{tscene};
+        geom.GetData(data);
+    }
+
+    auto triangles = std::make_unique<gl::TriangleMesh>(point_count);
+    triangles->LoadData(data);
+
+    gl::Scene scene{};
+    scene.AddObject(std::move(triangles));
     window.DrawFrames(renderer, scene, camera);
 
     return 0;
