6a18e00a3c87de8f3989c19a7a03b4485787dd06
Yaroslav Kuznetsov <asklater@bk.ru>
2025-03-04 18:52:26 +0300

    refactored and fixed exception safity

--- include/scene.hpp
+++ include/scene.hpp
@@ -1,35 +1,32 @@
 #pragma once
+
 #include "geometry.hpp"
 #include "octotree.hpp"
 #include "real_nums.hpp"
+#include "gl.hpp"
 #include <cassert>
 
 #include <iostream>
 #include <glm/gtc/epsilon.hpp>
 
 namespace scene {
-
-    class Scene {
+    class TriangleScene final {
     public:
-        virtual void Make() = 0;
-        virtual ~Scene() {}
-    }; // class Scene
-
-    class TriangleScene final : public Scene {
-    public:
-        void Make() override {
+        TriangleScene() {
             size_t fig_count = 0;
             std::cin >> fig_count;
-            point_count_ = fig_count * 3;
-
-            points_.reserve(point_count_);
-
             if (!std::cin.good())
-                throw std::runtime_error("input is failed");
+                throw std::runtime_error("Input is failed");
+            
+            point_count_ = fig_count * 3;
+            points_.reserve(point_count_);
             
             for (size_t i = 0; i < point_count_; ++i) {
                 float x, y, z;
                 std::cin >> x >> y >> z;
+                if (!std::cin.good())
+                    throw std::runtime_error("Input is failed");
+
                 points_.emplace_back(x, y, z);
             }
         }
@@ -43,6 +40,8 @@ namespace scene {
                     min_point = glm::min(min_point, point);
                     max_point = glm::max(max_point, point);
                 }
+                max_point_ = max_point;
+                min_point_ = min_point;
                 center_ = glm::vec3(min_point + max_point) * 0.5f;
             }
             
@@ -67,31 +66,95 @@ namespace scene {
             return point_count_;
         }
 
+        float GetMax() const {
+            return glm::max(glm::max(max_point_.x, max_point_.y), max_point_.z);
+        }
+
+        float GetMin() const {
+            return glm::min(glm::min(min_point_.x, min_point_.y), min_point_.z);
+        }
+
+        float GetNearestDistanceFrom(const glm::vec3 &src) const {
+            auto nearest = GetNearestFrom(src);
+            return glm::length(nearest - src);
+        }
+
+        float GetFarestDistanceFrom(const glm::vec3 &src) const {
+            auto farest = GetFarestFrom(src);
+            return glm::length(farest - src);
+        }
+
+        glm::vec3 GetNearestFrom(const glm::vec3 &src) const {
+            float distance = glm::length(points_[0] - src);
+            glm::vec3 dst{0.0f};
+
+            for (const auto &point : points_) {
+                auto tmp = glm::length(point - src);
+                if (distance > tmp) {
+                    distance = tmp;
+                    dst = point;
+                }
+            }
+            return dst;
+        }
+
+        glm::vec3 GetFarestFrom(const glm::vec3 &src) const {
+            float distance = 0.0f;
+            glm::vec3 dst{0.0f};
+
+            for (const auto &point : points_) {
+                auto tmp = glm::length(point - src);
+                if (distance < tmp) {
+                    distance = tmp;
+                    dst = point;
+                }
+            }
+            return dst;
+        }
+
     private:
         float radius_ = 0.0f;
         glm::vec3 center_ = glm::vec3(0.0f);
+        glm::vec3 max_point_ = glm::vec3(0.0f);
+        glm::vec3 min_point_ = glm::vec3(0.0f);
         size_t point_count_ = 0;
         std::vector<glm::vec3> points_;
     }; // class TriangleScene
 
     class GeometryData final {
     public:
-        GeometryData(const TriangleScene &scene) {
-            size_t point_count = scene.GetPointCount();
-            size_t fig_count = point_count / 3;
-            auto points = scene.GetPoints();
+        GeometryData(const TriangleScene &scene) : point_count_(scene.GetPointCount()) {
+            size_t fig_count = point_count_ / 3;
+
+            coords_.reserve(point_count_);
+            colors_.reserve(fig_count);
+            normals_.reserve(fig_count);
+            
             std::vector<geometry::figure_t<float>> figs;
-            std::set<size_t> intersected_figs;
+            auto points = scene.GetPoints();
+            CreateFigsAndSetCoords(points, figs);
+            SetColorsAndNormals(points, figs);
+        }
 
-            coords_.reserve(3 * point_count);
-            colors_.reserve(point_count);
-            normals_.reserve(point_count);
-            figs.reserve(fig_count);
+        void GetData(std::vector<gl::Vertex> &vertices) const {
+            vertices.reserve(point_count_);
+            
+            size_t fig_count = point_count_ / 3;
+            for (size_t i = 0; i < fig_count; ++i)
+                for (size_t j = 0; j < 3; ++j)
+                    vertices.emplace_back(coords_[3 * i + j], colors_[i], normals_[i]);
 
-            for (size_t i = 0; i < point_count; i += 3) {
-                coords_.push_back(points[i + 0].x); coords_.push_back(points[i + 0].y); coords_.push_back(points[i + 0].z);
-                coords_.push_back(points[i + 1].x); coords_.push_back(points[i + 1].y); coords_.push_back(points[i + 1].z);
-                coords_.push_back(points[i + 2].x); coords_.push_back(points[i + 2].y); coords_.push_back(points[i + 2].z);
+            assert(vertices.size() == point_count_);
+        }
+
+    private:
+        void CreateFigsAndSetCoords(const std::vector<glm::vec3> points, std::vector<geometry::figure_t<float>> &figs) {
+            figs.reserve(point_count_ / 3);
+
+            for (size_t i = 0; i < point_count_; i += 3) {
+                coords_.push_back(points[i + 0]);
+                coords_.push_back(points[i + 1]);
+                coords_.push_back(points[i + 2]);
                 
                 geometry::point_t<float> point1{points[i + 0].x, points[i + 0].y, points[i + 0].z};
                 geometry::point_t<float> point2{points[i + 1].x, points[i + 1].y, points[i + 1].z};
@@ -101,51 +164,37 @@ namespace scene {
                 figs.push_back(fig);
             }
 
+            assert(3 * figs.size() == point_count_);
+        }
+
+        void SetColorsAndNormals(const std::vector<glm::vec3> points, std::vector<geometry::figure_t<float>> &figs) {
+            std::set<size_t> intersected_figs;
             octotree::intersect_figs<float>(figs, intersected_figs);
             
             auto it = intersected_figs.begin();
             auto end = intersected_figs.end();
 
-            for (size_t i = 0; i < fig_count; ++i) {
+            for (size_t i = 0; i < point_count_ / 3; ++i) {
                 if (it != end && *it == i) {
                     ++it;
-                    colors_.push_back(1.0f); colors_.push_back(0.0f); colors_.push_back(0.0f);
+                    colors_.emplace_back(1.0f, 0.0f, 0.0f);
                 } else {
-                    colors_.push_back(0.0f); colors_.push_back(0.0f); colors_.push_back(1.0f);
+                    colors_.emplace_back(0.0f, 0.0f, 1.0f);
                 }
 
                 auto AB = points[3 * i + 1] - points[3 * i + 0];
                 auto AC = points[3 * i + 2] - points[3 * i + 0];
 
-                auto normal = glm::normalize(glm::cross(AB, AC));
-                normals_.push_back(normal.x); normals_.push_back(normal.y); normals_.push_back(normal.z);
+                normals_.push_back(glm::normalize(glm::cross(AB, AC)));
             }
-            
+
             assert(normals_.size() == colors_.size());
-            assert(normals_.size() == 3 * fig_count);
-            assert(coords_.size() == 9 * fig_count);
+            assert(coords_.size() == 3 * colors_.size());
         }
 
-        void GetData(std::vector<float> &data) const {
-            size_t point_count = coords_.size() / 3;
-            size_t fig_count = point_count / 3;
-            data.reserve(9 * point_count);
-
-            for (size_t i = 0; i < fig_count; ++i) {
-                for (size_t j = 0; j < 3; ++j) {
-                    data.push_back(coords_[9 * i + 3 * j + 0]); data.push_back(coords_[9 * i + 3 * j + 1]); data.push_back(coords_[9 * i + 3 * j + 2]);
-                    
-                    data.push_back(colors_[3 * i + 0]); data.push_back(colors_[3 * i + 1]); data.push_back(colors_[3 * i + 2]);         
-                    data.push_back(normals_[3 * i + 0]); data.push_back(normals_[3 * i + 1]); data.push_back(normals_[3 * i + 2]);
-                }
-            }
-
-            assert(data.size() == 3 * coords_.size());
-            assert(point_count == colors_.size());
-        }
-    private:
-        std::vector<float> coords_;
-        std::vector<float> colors_;
-        std::vector<float> normals_;
+        std::vector<glm::vec3> coords_;
+        std::vector<glm::vec3> colors_;
+        std::vector<glm::vec3> normals_;
+        size_t point_count_;
     };
 }
